20def040bc714d22e4f8c2576b18cc75c0c4de88
slonak79 <alsorto@gmail.com>
2014-03-05 15:40:30 -0800

    get tweets from twitter
    updated added file 3/5/2014

---
+++ test.py
@@ -0,0 +1,65 @@
+import oauth2 as oauth
+import urllib2 as urllib
+
+# See Assginment 6 instructions or README for how to get these credentials
+access_token_key = "277176492-nWAWzJ4XcLjbCDZycv1dGr9xYGAeq0dWr3hQoTrE"
+access_token_secret = "Jv7RS0Rk0EhlhVVSNQLEDAa883Fa9LgEFtM5w3qu6yoPI"
+
+consumer_key = "A1ZvJ87SDhGXO4T0dCHBw"
+consumer_secret = "iYOonEQm2KwsVAOYeHR3p700OU69pfLLp9QO6acMs"
+
+_debug = 0
+
+oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
+oauth_consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
+
+signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+
+http_method = "GET"
+
+
+http_handler  = urllib.HTTPHandler(debuglevel=_debug)
+https_handler = urllib.HTTPSHandler(debuglevel=_debug)
+
+'''
+Construct, sign, and open a twitter request
+using the hard-coded credentials above.
+'''
+def twitterreq(url, method, parameters):
+  req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                             token=oauth_token,
+                                             http_method=http_method,
+                                             http_url=url, 
+                                             parameters=parameters)
+
+  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+
+  headers = req.to_header()
+
+  if http_method == "POST":
+    encoded_post_data = req.to_postdata()
+  else:
+    encoded_post_data = None
+    url = req.to_url()
+
+  opener = urllib.OpenerDirector()
+  opener.add_handler(http_handler)
+  opener.add_handler(https_handler)
+
+  response = opener.open(url, encoded_post_data)
+
+  return response
+
+def fetchsamples():
+  #url = "https://api.twitter.com/1.1/geo/search.json/track=locations=-180,-90,180,90"
+  url = "https://api.twitter.com/1.1/geo/search.json?query=Toronto,Dallas"
+  #url = "https://stream.twitter.com/1/statuses/firehose.json"
+  
+  #can change the url to the newer REST API v. 1.1 instead
+  parameters = []
+  response = twitterreq(url, "POST", parameters)
+  for line in response:
+    print line.strip()
+
+if __name__ == '__main__':
+  fetchsamples()
